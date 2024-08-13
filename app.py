b020eeb6b9c3304764da3cc69d7ea0b3922d5794
Gavin Shaw <gavshaw+github@gmail.com>
2024-08-13 01:02:42 -0400

    Cleanup style of variable naming

--- app.py
+++ app.py
@@ -4,44 +4,46 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/generate', methods=['POST'])
 def generate_password():
     length = request.json.get('length')
-    startWithLetterOrNumber = request.json.get('startWithLetterOrNumber')
+    start_with_letter_or_number = request.json.get('startWithLetterOrNumber')
 
-    lowercaseletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                        't', 'u', 'v', 'w', 'x', 'y', 'z']
+    lower_case_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                          't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    uppercaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                        'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    upper_case_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                          'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-    specialcharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '?']
+    special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '?']
 
-    generatedpassword = ""
+    generated_password = ""
 
     for i in range(int(length)):
-        if i == 0 and bool(startWithLetterOrNumber):
+        if i == 0 and bool(start_with_letter_or_number):
             # First char of password and must start with letter/number
-            charType = random.randint(0,2)
+            char_type = random.randint(0, 2)
         else:
             # Can be a special char
-            charType = random.randint(0, 3)
+            char_type = random.randint(0, 3)
 
-        match charType:
+        match char_type:
             case 0:
                 # Lowercase letter
-                generatedpassword = generatedpassword + random.choice(lowercaseletters)
+                generated_password = generated_password + random.choice(lower_case_letters)
             case 1:
                 # Uppercase letter
-                generatedpassword = generatedpassword + random.choice(uppercaseletters)
+                generated_password = generated_password + random.choice(upper_case_letters)
             case 2:
                 # Number
-                generatedpassword = generatedpassword + str(random.randint(0, 9))
+                generated_password = generated_password + str(random.randint(0, 9))
             case 3:
                 # Special character
-                generatedpassword = generatedpassword + random.choice(specialcharacters)
+                generated_password = generated_password + random.choice(special_characters)
 
-    return generatedpassword
+    return generated_password
+
 
 if __name__ == '__main__':
     app.run()
